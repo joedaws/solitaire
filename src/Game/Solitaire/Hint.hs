7ad8255bc0d172fa9427c692854ddc4939e084c4
Joe <daws.joseph@gmail.com>
2025-05-19 00:16:41 -0400

    feat(src): added hint2 function for next two states

--- src/Game/Solitaire/Hint.hs
+++ src/Game/Solitaire/Hint.hs
@@ -1,5 +1,6 @@
 module Game.Solitaire.Hint (
     hint,
+    hint2,
 ) where
 
 import Game.Card
@@ -11,3 +12,8 @@ hint s = filter differentState allNextStates
   where
     differentState s' = s /= s'
     allNextStates = map ($ s) allTransitions
+
+hint2 :: (HasFace c, HasCard c, Eq c, Show c, IsPlayable c) => Solitaire c -> [Solitaire c]
+hint2 start = do
+    first <- hint start
+    hint first
